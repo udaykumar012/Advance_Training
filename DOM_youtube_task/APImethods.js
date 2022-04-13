8d1958eb9a681e0caa3363d75e8de7f89e0af478
udaykumar012 <uday_kumar_kotla@epam.com>
2022-04-13 11:48:28 +0530

    added some details

--- DOM_youtube_task/APImethods.js
+++ DOM_youtube_task/APImethods.js
@@ -10,13 +10,48 @@ async function callApi(searchval){
         .then(d=>{data=d})
         .catch(err=>console.log(err));
         //console.log(data)
+    videocontainer=document.createElement('div');
+    videocontainer.setAttribute('class','videocontainer')
+    Main.appendChild(videocontainer);
     for(var i=0;i<data.items.length;i++){
-        var playlistimg=document.createElement('img');
-        var playlistimgsrc=data.items[i].snippet.thumbnails.high.url;
-        console.log(playlistimgsrc);
-        playlistimg.setAttribute('src',playlistimgsrc);
-        playlistimg.setAttribute('class','images');
-       Main.appendChild(playlistimg);
+        video=document.createElement('div');
+        video.setAttribute('class','video');
+        videocontainer.appendChild(video);
+        videoimgdiv=document.createElement("div");
+        video.appendChild(videoimgdiv);
+        videoimg=document.createElement('img');
+        imgurl=data.items[i].snippet.thumbnails.high.url;
+        //console.log(imgurl);
+        videoimg.setAttribute('src',imgurl);
+        videoimg.setAttribute('class',"videosnap")
+        videoimgdiv.appendChild(videoimg);
+        videodescdiv=document.createElement("div");
+        video.appendChild(videodescdiv);
+
+        list_ul=document.createElement("ul");
+        videodescdiv.appendChild(list_ul);
+        tilte_li=document.createElement('li');
+        tilte_li.setAttribute("class","Title");
+        videourl=document.createElement("a");
+        var youtubeurl="https://www.youtube.com/watch?v="+data.items[i].id.videoId;
+        
+        videourl.setAttribute("href",youtubeurl);
+        videourl.innerHTML=data.items[i].snippet.title;
+        publishedtime_li=document.createElement('li');
+        publishedtime_li.setAttribute("class","publishTime");
+        publishedtime_li.innerHTML=data.items[i].snippet.publishTime;
+        tilte_li.appendChild(videourl);
+        //console.log(data.items[i].snippet.publishTime)
+        channeltitle_li=document.createElement('li');
+        channeltitle_li .setAttribute("class","channelTitle");
+        channeltitle_li .innerHTML=data.items[i].snippet.channelTitle;
+        description_li=document.createElement('li');
+        description_li .setAttribute("class","description");
+        description_li.innerHTML=data.items[i].snippet.description;
+        list_ul.appendChild(tilte_li);
+        list_ul.appendChild(publishedtime_li);
+        list_ul.appendChild(channeltitle_li);
+        list_ul.appendChild(description_li);
     }
     
         
